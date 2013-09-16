b8e657d9b1e11e65dc238ed3823bcb9e9d8cd4ff
Denaildo <denaildojr@hotmail.com>
2013-09-16 20:29:16 -0300

    upload autenticacao parte 1

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :role
+  # attr_accessible :title, :body
+  validates_presence_of :username
+    
+    ROLES = %w[administrador tecnico docente aluno]
+    
+    def role_symbols
+        [role.to_sym]
+    end
+end

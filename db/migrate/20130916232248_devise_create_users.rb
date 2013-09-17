5fd32e21495333803b73c3d92e4ae581b66927c9
imbrito <imbrito19rj@hotmail.com>
2013-09-16 21:37:18 -0300

    upload autenticacao parte 4

--- db/migrate/20130916232248_devise_create_users.rb
+++ db/migrate/20130916232248_devise_create_users.rb
@@ -2,10 +2,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-       t.string :email,              :null => false
-       t.string :encrypted_password, :null => false
-       t.string :username,           :null => false
-       t.string :role,               :null => false
+      t.string :email,              :null => false
+      t.string :encrypted_password, :null => false
+      t.string :username,           :null => false
+      t.string :role,               :null => false
 
       ## Recoverable
       t.string   :reset_password_token
